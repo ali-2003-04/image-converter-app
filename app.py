2b9069a52d2720ebeba15db8ee4e929b5902e488
Winx <ali.belyoum.68@edu.uiz.ac.ma>
2024-12-25 14:08:14 +0100

    Prepare for Vercel deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, send_file
 from PIL import Image
 import io
@@ -36,4 +37,4 @@ def convert():
         return str(e), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
